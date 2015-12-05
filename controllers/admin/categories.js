be88ddaed2361708133b851adfa5eb057078354b
Alexander Ostach <ukrorion@gmail.com>
2015-12-05 05:21:42 +0300

    Added new stuff to categories index template

--- controllers/admin/categories.js
+++ controllers/admin/categories.js
@@ -3,7 +3,7 @@ var categoriesAdminController = function(wagner){
   var index = wagner.invoke(function(Category){
     return function(req, res, next){
       Category.find({},function(err,docs){
-        res.render('admin/categories/index');
+        res.render('admin/categories/index', { categories:docs });
       });
     }
   });
