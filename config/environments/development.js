b56f1b37e19af1a68589358454588cf9c7b6a345
Alexander Ostach <ukrorion@gmail.com>
2015-12-07 00:32:20 +0300

    Share common environment

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,10 +1,6 @@
-var bodyParser = require('body-parser');
+var common_env = require('../../config/environments/common');
 
 module.exports = function(app){
-  app.use(require('morgan')("combined"));
-  app.set('views', './views');
-  app.set('view engine', 'jade');
-  app.use(bodyParser.json()); // support json encoded bodies
-  app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+  common_env(app);
 };
 
