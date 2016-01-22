9dbe6c092bf583a3bde9c541c4700560ee8f3049
Alexander Ostach <ukrorion@gmail.com>
2016-01-22 03:27:50 +0200

    Added sign up form for user

--- public/javascripts/app/controllers/navigation_controller.js
+++ public/javascripts/app/controllers/navigation_controller.js
@@ -3,7 +3,8 @@ angular.module('edx-app')
     $scope.links = [
       {name: "Home", url:"/"},
       {name: "Blog", url:"/blogs"},
-      {name: "News", url:"/news"}
+      {name: "News", url:"/news"},
+      {name: "Sing Up", url:"/sing_up"}
     ];
     $scope.is_active = function(path){
       return path === $location.path();
---
+++ public/javascripts/app/controllers/sign_up_controller.js
@@ -0,0 +1,14 @@
+angular.module('edx-app')
+  .controller('sign_up_controller', function($scope) {
+    $scope.email = '';
+    $scope.password = '';
+    $scope.first_name = '';
+    $scope.last_name = '';
+    $scope.submit = function(){
+      $scope.info = 'Form was submited!'
+      $scope.show_alert = true;
+    }
+    $scope.hide_alert = function(){
+      $scope.show_alert = false;
+    }
+  })
