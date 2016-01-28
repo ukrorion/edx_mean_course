2fb9187ac000fddce5f3a12584ceb4390dde9a51
Alexander Ostach <ukrorion@gmail.com>
2016-01-28 12:39:07 +0200

    Removed session stuff

--- config/environments/common.js
+++ config/environments/common.js
@@ -1,19 +1,11 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-var session = require('express-session');
-var session_helper = require('../../lib/session');
 
 module.exports = function(app){
   app.use(express.static(process.cwd() + '/public'));
   app.use(require('morgan')("combined"));
-  app.use(session({
-                    secret: 'keyboard cat',
-                    resave: false,
-                    saveUninitialized: true
-                  }));
   app.set('views', './views');
   app.set('view engine', 'jade');
   app.use(bodyParser.json()); // support json encoded bodies
   app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-  app.use(session_helper);
 };
